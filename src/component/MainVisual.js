8a05238d831e18f88b5f387bdcabd9bb8c60b924
jigooo <h00kk@naver.com>
2022-09-21 17:44:55 +0900

    sponsor

--- src/component/MainVisual.js
+++ src/component/MainVisual.js
@@ -1,39 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import { Autoplay } from "swiper";
 
 const DB = [
   {
     id: 1,
-    content:
-      "BUILDING A FUTURE IN\nWHICH\nPEOPLE LIVE IN HARMONY \nWITH NATURE",
+    content: "BUILDING A FUTURE IN\nWHICH\nPEOPLE LIVE IN HARMONY\nWITH NATURE",
     desc: "WWF와 함께 만드는\n사람과 자연이 조화로운 미래",
   },
   {
     id: 2,
-    content:
-      "BUILDING A FUTURE IN\nWHICH\nPEOPLE LIVE IN HARMONY \nWITH NATURE",
+    content: "BUILDING A FUTURE IN\nWHICH\nPEOPLE LIVE IN HARMONY\nWITH NATURE",
     desc: "WWF와 함께 만드는\n사람과 자연이 조화로운 미래",
   },
   {
     id: 3,
-    content:
-      "BUILDING A FUTURE IN\nWHICH\nPEOPLE LIVE IN HARMONY \nWITH NATURE",
+    content: "BUILDING A FUTURE IN\nWHICH\nPEOPLE LIVE IN HARMONY\nWITH NATURE",
     desc: "WWF와 함께 만드는\n사람과 자연이 조화로운 미래",
   },
   {
     id: 4,
-    content:
-      "BUILDING A FUTURE IN\nWHICH\nPEOPLE LIVE IN HARMONY \nWITH NATURE",
+    content: "BUILDING A FUTURE IN\nWHICH\nPEOPLE LIVE IN HARMONY\nWITH NATURE",
     desc: "WWF와 함께 만드는\n사람과 자연이 조화로운 미래",
   },
   {
     id: 5,
-    content:
-      "BUILDING A FUTURE IN\nWHICH\nPEOPLE LIVE IN HARMONY \nWITH NATURE",
+    content: "BUILDING A FUTURE IN\nWHICH\nPEOPLE LIVE IN HARMONY\nWITH NATURE",
     desc: "WWF와 함께 만드는\n사람과 자연이 조화로운 미래",
   },
 ];
+
 
 const MainVisual = () => {
   const [idx, setIDX] = useState();
@@ -46,25 +43,33 @@ const MainVisual = () => {
 
   return (
     <section className="MainVisual">
-  <Swiper style={swiperStyle} loop={true} className="mainVisual">
-      {DB.map((slide) => (
-        <SwiperSlide>
-          <figure className={"item0" + slide.id}>
-            <div className="container">
-            <div className="visual_text">
-              <h2>{slide.content}</h2>
-              <p>{slide.desc}</p>
-              <span>
-                <a href="/">MORE</a>
-              </span>
-            </div>
-            </div>
-          </figure>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+      <Swiper
+        style={swiperStyle}
+        loop={true}
+        autoplay={{
+          delay: 7000,
+          disableOnInteraction: true,
+        }}
+        modules={[Autoplay]}
+        className="mainVisual"
+      >
+        {DB.map((slide) => (
+          <SwiperSlide>
+            <figure className={"item0" + slide.id}>
+              <div className="container">
+                <div className="visual_text">
+                  <h2>{slide.content}</h2>
+                  <p>{slide.desc}</p>
+                  <span>
+                    <a href="/">MORE</a>
+                  </span>
+                </div>
+              </div>
+            </figure>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </section>
-  
   );
 };
 
--- src/component/WWFSponsor.js
+++ src/component/WWFSponsor.js
@@ -6,14 +6,18 @@ const DB = [
   {
     id: 1,
     content: "박서준",
+    info: "배우",
     desc: "“ 전 세계적 곳곳에서 벌어지는 대규모 화재, 더위, 가뭄, 홍수 등 계속되는 자연재해는 점점 거대한 두려움으로 다가오고 있습니다. 지속가능한 지구를 위한 WWF의 활동에 응원을 보내며, 기후위기가 더욱 심각해지는 만큼 앞으로 더 많은 분들이 변화에 동참해 주시길 바라는 마음입니다. “",
   },
   {
     id: 2,
     content: "타일러 라쉬",
+    info: "방송인·작가",
     desc: "“인간인 저의 존재가 문제를 야기하는 것이 아닌 해결책의 일부가 될 수 있었으면 좋겠다는 생각이죠. 심각한 기후위기를 겪고있는 상황에서 많은 후원자 분들이 지구의 귀한 생명과 생태계를 지켜주는 데 힘을 모아주고 있습니다. 기후위기 문제가 어렵고 복잡해도, 함께라면 해결할 수 있겠다는 희망을 얻게 되는 것 같아요.“",
   },
 ];
+
+const SB = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }];
 
 const WWFSponsor = () => {
   const swiperStyle = {};
@@ -23,7 +27,7 @@ const WWFSponsor = () => {
       <div className="container">
         <div className="sponsor_title">
           <h2>WWF-KOREA 홍보대사</h2>
-          <p>
+          <p className="sponsor_title_tit">
             WWF-Korea 홍보대사 및 WWF 글로벌 홍보대사는 멸종위기종 및 서식지
             보전, 기후위기 대응을 위한 WWF의 자연보전 활동을 널리 알리고,{" "}
             <strong>
@@ -32,12 +36,24 @@ const WWFSponsor = () => {
             </strong>
           </p>
         </div>
-        <Swiper className="sponsor" style={swiperStyle} loop={true}>
+        <Swiper className="sponsor" style={swiperStyle}>
           {DB.map((slide) => (
             <SwiperSlide>
               <figure className={"item0" + slide.id}>
-                <span>{slide.content}</span>
-                <p>{slide.desc}</p>
+                <strong className="sponsor_name">
+                  {slide.content}
+                  <span className="sponsor_info">{slide.info}</span>
+                </strong>
+                <p className="sponsor_tit">{slide.desc}</p>
+                <Swiper loop={true} className="sponsorPT">
+                 {
+                  SB.map((sld) => (
+                    <SwiperSlide>
+                        <img src={process.env.PUBLIC_URL + "/assets/images/sponsor0" + slide.id + "_t0" + sld.id + ".png"} alt="" />
+                    </SwiperSlide>
+                  ))
+                 }
+                </Swiper>
               </figure>
             </SwiperSlide>
           ))}
